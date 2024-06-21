344f784d3d404bfcd2cf336ed62a532bc7641ab6
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2024-06-21 11:40:16 +0600

    middleware added

--- src/middleware.js
+++ src/middleware.js
@@ -1,3 +1,22 @@
-export const middleware = () => {
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
+
+export const middleware = (request) => {
+    const token = cookies(request).get('next-auth.session-token');
+
+    const pathName = request.nextUrl.pathName;
+    if (pathName.includes("api")) return NextResponse.next();
+
+    console.log(token);
     console.log('middleware');
-}
+    if (!token) {
+        return NextResponse.redirect(new URL('/login', request.url));
+    }
+    return NextResponse.next();
+};
+
+export const config = {
+    matcher: [
+        '/myBookings/:path*', "/checkout", '/services/:path*'
+    ]
+};
